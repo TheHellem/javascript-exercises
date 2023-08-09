5e55fdd670d68c8acf6463568242b661f67892ed
HenrikH <henrik.hellem@live.com>
2023-08-09 15:15:49 +0200

    finish nr 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+    return array.filter((item) => !items.includes(item) )
 
 };
+console.log(removeFromArray([1, 2, 3, 4], 3, 4))
 
 // Do not edit below this line
 module.exports = removeFromArray;
