4783884da446769500984785f88a7c95834e975f
HenrikH <henrik.hellem@live.com>
2023-08-09 14:34:31 +0200

    Finish 2 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+  if (number > 0) return string.repeat(number);
+  if (number < 0) return "ERROR"
+  else return "";
 };
+
+console.log(repeatString("hey", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
