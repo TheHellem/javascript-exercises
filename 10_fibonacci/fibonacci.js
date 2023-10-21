b36aa2f9deb106a6ec177b12e1e38398df27e698
HenrikH <henrik.hellem@live.com>
2023-10-21 16:36:30 +0200

    finish 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
 
+
+const fibonacci = (n) => {
+    if(n < 0) {
+        return "OOPS"
+    } else if (n == 0){
+        return 0;
+    }
+    else {
+        return n <= 2 ? 1 : fibonacci(n - 1) + fibonacci(n - 2);
+    }
 };
 
 // Do not edit below this line
